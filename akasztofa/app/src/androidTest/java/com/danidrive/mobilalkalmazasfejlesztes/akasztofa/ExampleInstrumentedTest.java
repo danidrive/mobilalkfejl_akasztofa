24ade03ee7129acd3829b7d762351a1db3ee15d5
Daniel Kaposvari <kaposvaridani1@gmail.com>
2019-05-11 23:12:29 +0200

    Material Design

--- akasztofa/app/src/androidTest/java/com/danidrive/mobilalkalmazasfejlesztes/akasztofa/ExampleInstrumentedTest.java
+++ akasztofa/app/src/androidTest/java/com/danidrive/mobilalkalmazasfejlesztes/akasztofa/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package com.danidrive.mobilalkalmazasfejlesztes.akasztofa;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
